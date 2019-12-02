20e9ac92b869b3981418f922a26789ead06de585
atozprasad <atozcareer@gmail.com>
2019-12-02 21:28:51 +0100

    Creating a new pakcage in pyton

---
+++ python/nsxtapiconfreader/__init__.py
@@ -0,0 +1,1 @@
+from Configmap import Configmap
---
+++ python/nsxtapiconfreader/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="nsxtapiconfreader",  # Replace with your own username
+    version="0.0.1",
+    author="Prasad Challagandla",
+    author_email="atozcareer@gmail.com",
+    description="A small example package for NSXT API Configreader",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
