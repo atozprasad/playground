0ab029ce8a0a7835ff2d5fef7581060d2e800570
atozprasad <atozcareer@gmail.com>
2021-11-09 00:17:55 +0100

    script commands updated to incorporage path release name

--- vcf/esxi-update-patch.sh
+++ vcf/esxi-update-patch.sh
@@ -7,6 +7,13 @@ HOSTS=`cat $FILENAME`
 
 for ESXIHOSTNAME in $HOSTS
 do
+	### Trim Hostname string
+	ESXIHOSTNAME=`echo $ESXIHOSTNAME | sed -e 's/^[[:space:]]*//'`
+	### Ignoring any comments
+	if [ ${ESXIHOSTNAME}::1 == '#' ]  
+	then 
+		continue;
+	fi 
 	echo -e "\nEnter Password for ssh root@${ESXIHOSTNAME}"
 	echo "#Updating to ESXi-7.0U1d-17551050-standard "
 	SCRIPT="esxcli software profile update -p ESXi-7.0U1d-17551050-standard -d https://hostupdate.vmware.com/software/VUM/PRODUCTION/main/vmw-depot-index.xml; reboot -f " 
